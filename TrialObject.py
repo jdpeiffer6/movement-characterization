6231307c53a8fd5d0077a37509b48a047bb849ee
jdpeiffer6 <j.d@wustl.edu>
2022-02-07 15:04:09 -0600

    Updated gait metrics

--- TrialObject.py
+++ TrialObject.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 import mpld3
 from scipy import signal
+from scipy.interpolate import interp1d
+import numpy as np
 
 class TrialObject:
     def __init__(self, path):
@@ -29,6 +31,10 @@ class TrialObject:
         self.markerless_fs = 80
         self.marker_fs = 100
 
+        self.step_width = []   #TODO: delete these
+        self.step_length = []
+        self.step_height = []
+
         self.load_markerless()
         self.load_markers()
         # self.create_walking_plots()
@@ -37,6 +43,7 @@ class TrialObject:
 
     def load_markerless(self):
         self.markerless_data = {}
+        self.markerless_events = {}
         with open(glob.glob(self.markerless_folder+'/*.json')[0]) as f:
             markerless_data_json = json.load(f)
             self.markerless_json_path = glob.glob(self.markerless_folder+'/*.json')[0]
@@ -47,9 +54,15 @@ class TrialObject:
             trial_name = trial_name.split('/')[-1][0:-4]
             if trial_name not in self.markerless_data.keys():
                 self.markerless_data[trial_name] = pd.DataFrame()
+                self.markerless_events[trial_name] = {}
                 print(trial_name)
             for j in range(len(data['signal'])):
-                self.markerless_data[trial_name][data['name']+data['signal'][j]['component']] = data['signal'][j]['data']
+                if data['name'] in ['LHS','LTO','RHS','RTO']:
+                    self.markerless_events[trial_name].update({data['name']:data['signal'][j]['data']})
+                    #append to different container
+                    a=5
+                else:
+                    self.markerless_data[trial_name][data['name']+data['signal'][j]['component']] = data['signal'][j]['data']
         
         for i in self.markerless_data.keys():
             self.markerless_data[i].dropna(how='all',inplace=True)
@@ -169,59 +182,161 @@ class TrialObject:
         print("Completed walking html export")
 
     def analyze_walking(self):
+        self.calculate_step_height()
         self.calculate_step_width()
+        # self.calculate_step_width2()
+        
+
+    def interpolate_walking(self):
+        walking_keys = self.markerless_task_labels[self.markerless_task_labels['Task'] == 'Walking']['Name']
+        peakdict = {}
+        for i in range(len(walking_keys)):
+            data = self.markerless_data[walking_keys[i]]
+             #interpolate that sh(t)
+            # right leg
+            xmin = min(data['R_HEELX'])
+            xmax = max(data['R_HEELX'])
+            xnew = np.linspace(xmin,xmax,num = data['R_HEELX'].shape[0],endpoint=True)
+            x = data['R_HEELX']
+            z = data['R_HEELZ']
+            f_interp = interp1d(x,z)
+            r_znew = f_interp(xnew)
+            self.markerless_data[walking_keys[i]]['R_HEELZ_WRT_X'] = r_znew
+            r_peaks = signal.find_peaks(-1*r_znew,threshold = -0.05,distance=50)
+
+            # left leg
+            xmin = min(data['L_HEELX'])
+            xmax = max(data['L_HEELX'])
+            xnew = np.linspace(xmin,xmax,num = data['L_HEELX'].shape[0],endpoint=True)
+            x = data['L_HEELX']
+            z = data['L_HEELZ']
+            f_interp = interp1d(x,z)    #TODO: may want to add cubic interpolation but kinda too lazy rn
+            l_znew = f_interp(xnew)
+            self.markerless_data[walking_keys[i]]['L_HEELZ_WRT_X'] = l_znew
+            l_peaks = signal.find_peaks(-1*l_znew,threshold = -0.05,distance=50)
+
+            # t = np.linspace(0,x.shape[0]/100,num=x.shape[0])
+            # plt.subplot(2,2,1)
+            # plt.plot(t,data['L_HEELZ'],'b')
+            # plt.subplot(2,2,3)
+            # plt.plot(self.markerless_data[walking_keys[i]]['L_HEELZ_WRT_X'].values,'g')
+            # plt.scatter(l_peaks[0],self.markerless_data[walking_keys[i]]['L_HEELZ_WRT_X'].values[l_peaks[0]],c='r')
+            # plt.subplot(2,2,2)
+            # plt.plot(t,data['R_HEELZ'],'b')
+            # plt.subplot(2,2,4)
+            # plt.plot(self.markerless_data[walking_keys[i]]['R_HEELZ_WRT_X'].values,'g')
+            # plt.plot(self.markerless_data[walking_keys[i]]['R_HEELX'].values,self.markerless_data[walking_keys[i]]['R_HEELZ'].values,'y--')
+            # plt.scatter(r_peaks[0],self.markerless_data[walking_keys[i]]['R_HEELZ_WRT_X'].values[r_peaks[0]],c='r')
+            
+            # plt.suptitle("Interpolated")
+            # plt.show()
+            peakdict.update({walking_keys[i]:[r_peaks[0],l_peaks[0]]})
+        return peakdict
+            
 
     def calculate_step_width(self):
+        peakdict = self.interpolate_walking() # returns {key:[right,left]}
         walking_keys = self.markerless_task_labels[self.markerless_task_labels['Task'] == 'Walking']['Name']
         for i in range(len(walking_keys)):
             data = self.markerless_data[walking_keys[i]]
-            heel = data[['R_HEELX','R_HEELY','R_HEELZ','L_HEELX','L_HEELY','L_HEELZ']]
+            heel = data[['R_HEELX','R_HEELY','R_HEELZ','L_HEELX','L_HEELY','L_HEELZ','L_HEELZ_WRT_X','R_HEELZ_WRT_X']]
+            r_peaks = peakdict[walking_keys[i]][0]
+            l_peaks = peakdict[walking_keys[i]][1]
             f = plt.figure()
             gs= GridSpec(2,2,figure=f)
             ax1 = f.add_subplot(gs[0,0])
             ax2 = f.add_subplot(gs[0,1])
             ax3 = f.add_subplot(gs[1,:])
-
-            ax1.plot(heel['L_HEELX'].values,heel['L_HEELZ'].values)
-            peaks = signal.find_peaks(-1*heel['L_HEELZ'],threshold = -0.025,width=10)
-            ax1.plot(heel['L_HEELX'].values[peaks[0]],heel['L_HEELZ'].values[peaks[0]],'rx')
-            ax3.plot(heel['L_HEELX'].values,heel['L_HEELY'].values)
-            ax3.plot(heel['L_HEELX'].values[peaks[0]],heel['L_HEELY'].values[peaks[0]],'rx')
+            
+            ax1.plot(heel['L_HEELZ_WRT_X'].values)
+            ax1.scatter(l_peaks,heel['L_HEELZ_WRT_X'].values[l_peaks],c='r')
             ax1.set_ylabel('Lab Z (m)')
+            ax1.set_xlabel('Lab X (cm)')
 
-            ax2.plot(heel['R_HEELZ'].values,'g')
-            peaks = signal.find_peaks(-1*heel['R_HEELZ'],threshold = -0.025,width = 10)
-            ax2.plot(peaks[0],heel['R_HEELZ'].values[peaks[0]],'rx')
+            ax2.plot(heel['R_HEELZ_WRT_X'].values,'g')
+            ax2.scatter(r_peaks,heel['R_HEELZ_WRT_X'].values[r_peaks],c='r')
+            ax2.set_ylabel('Lab Z (m)')
+            ax2.set_xlabel('Lab X (m)')
+            ax3.plot(heel['L_HEELX'].values,heel['L_HEELY'].values)
+            ax3.scatter(heel['L_HEELX'].values[l_peaks],heel['L_HEELY'].values[l_peaks],c='r')
+
             ax3.plot(heel['R_HEELX'].values,heel['R_HEELY'].values,'g')
-            ax3.plot(heel['R_HEELX'].values[peaks[0]],heel['R_HEELY'].values[peaks[0]],'rx')
+            ax3.scatter(heel['R_HEELX'].values[r_peaks],heel['R_HEELY'].values[r_peaks],c='r')
             ax3.set_xlabel('Lab X (m)')
             ax3.set_ylabel('Lab Y (m)')
             ax3.legend(['Left','Heel Down','Right'])
 
-            #TODO: figure out how to find peaks when scaled by x
-            # ax[0][0].plot(heel['L_HEELX'].values)
-            # peaks = signal.find_peaks(heel['L_HEELX'])
-            # ax[0][0].plot(peaks[0],heel['L_HEELX'].values[peaks[0]],'x')
-
-            # ax[1][0].plot(heel['L_HEELY'].values)
-            # peaks = signal.find_peaks(heel['L_HEELY'])
-            # ax[1][0].plot(peaks[0],heel['L_HEELY'].values[peaks[0]],'x')
-
-            # ax[0][1].plot(heel['R_HEELX'].values)
-            # peaks = signal.find_peaks(heel['R_HEELX'])
-            # ax[0][1].plot(peaks[0],heel['R_HEELX'].values[peaks[0]],'x')
-
-            # ax[1][1].plot(heel['R_HEELY'].values)
-            # peaks = signal.find_peaks(heel['R_HEELY'])
-            # ax[1][1].plot(peaks[0],heel['R_HEELY'].values[peaks[0]],'x')
-
-            # ax[2][1].plot(heel['R_HEELZ'].values)
-            # peaks = signal.find_peaks(-1*heel['R_HEELZ'],threshold = -0.025,width = 20)
-            # ax[2][1].plot(peaks[0],heel['R_HEELZ'].values[peaks[0]],'x')
-
+            #TODO: scaling peaks by x has an issue it seems...maybe use cubic interpolation?
+            for _ in range(r_peaks.size+l_peaks.size):
+                if r_peaks.size ==0 or l_peaks.size ==0:
+                    break
+                r_min = np.min(r_peaks)
+                l_min = np.min(l_peaks)
+                plt.plot([heel['R_HEELX'].values[r_peaks[0]],heel['L_HEELX'].values[l_peaks[0]]], [heel['R_HEELY'].values[r_peaks[0]],heel['L_HEELY'].values[l_peaks[0]]], 'yo', linestyle="--")
+                self.step_length.append(np.abs(heel['R_HEELX'].values[r_peaks[0]]-heel['L_HEELX'].values[l_peaks[0]]))
+                self.step_width.append(np.abs(heel['R_HEELY'].values[r_peaks[0]]-heel['L_HEELY'].values[l_peaks[0]]))
+                if r_min < l_min:
+                    r_peaks = np.delete(r_peaks,0)
+                else:
+                    l_peaks = np.delete(l_peaks,0)
+            print("Step Width: %.3f +/- (%.3f)"%(np.mean(self.step_width),np.std(self.step_width)))
+            print("Step length: %.3f +/- (%.3f)"%(np.mean(self.step_length),np.std(self.step_length)))
+            plt.suptitle(walking_keys[i])
             plt.show()
             del f,ax1,ax2,ax3
             plt.close()
+
+    def calculate_step_width2(self):
+        trial_names = list(self.markerless_events.keys())
+        for trial in trial_names:
+            data = self.markerless_data[trial]
+            rhs = np.array(self.markerless_events[trial]['RHS'])
+            lhs = np.array(self.markerless_events[trial]['LHS'])
+            rto = np.array(self.markerless_events[trial]['RTO'])
+            lto = np.array(self.markerless_events[trial]['LTO'])
+            rhs = np.round(rhs*100).astype(int)
+            lhs = np.round(lhs*100).astype(int)
+            rto = np.round(rto*100).astype(int)
+            lto = np.round(lto*100).astype(int)
+            max_ind = np.max(data.index)
+            rhs = rhs[:np.argmax(rhs>=max_ind)]
+            lhs = lhs[:np.argmax(lhs>=max_ind)]
+            rto = rto[:np.argmax(rto>=max_ind)]
+            lto = rhs[:np.argmax(lto>=max_ind)]
+
+            plt.plot(data['R_HEELX'],data['R_HEELY'],'b',data['L_HEELX'],data['L_HEELY'],'g')
+            plt.scatter(data['R_HEELX'][rto],data['R_HEELY'][rto],c='r')
+            plt.scatter(data['L_HEELX'][lto],data['L_HEELY'][lto],c='y')
+            print(trial)
+            print(rhs)
+            print(lhs)
+            plt.show()
+    
+
+    def calculate_step_height(self):
+        walking_keys = self.markerless_task_labels[self.markerless_task_labels['Task'] == 'Walking']['Name']
+        for i in range(len(walking_keys)):
+            data = self.markerless_data[walking_keys[i]]
+            # L Heel
+            plt.subplot(1,2,1)
+            plt.plot(data['L_HEELZ'].values)
+            l_peaks = signal.find_peaks(data['L_HEELZ'].values,height=0.1)[0]
+            plt.scatter(l_peaks,data['L_HEELZ'].values[l_peaks],c='r')
+
+            #R Heel
+            plt.subplot(1,2,2)
+            plt.plot(data['R_HEELZ'].values)
+            r_peaks = signal.find_peaks(data['R_HEELZ'].values,height=0.1)[0]
+            plt.scatter(r_peaks,data['R_HEELZ'].values[r_peaks],c='r')
+            plt.suptitle(walking_keys[i])
+            
+            for j in l_peaks:
+                self.step_height.append(data['L_HEELZ'].values[j])
+            for j in r_peaks:
+                self.step_height.append(data['R_HEELZ'].values[j])
+            print("Step height: %.3f +/- (%.3f)"%(np.mean(self.step_height),np.std(self.step_height)))
+            plt.show()
+
 
     def create_tandem_plots(self):
         self.colors = ['#130AF1','#17ACE8','#1BD9DE']
--- analysis.py
+++ analysis.py
@@ -2,7 +2,7 @@
 import os
 import TrialObject
 import matplotlib.pyplot as plt
-from scipy.misc import derivative
+import numpy as np
 
 from TrialObject import TrialObject
 path_to_trial = "C:\\Users\\jd\\Box\\Movement-Characterization\\data\\output\\002\\2022-01-28"
@@ -11,11 +11,15 @@ trial_obj = TrialObject(path_to_trial)
 # %%
 data = trial_obj.markerless_data['Gait Markerless 1']
 pelvisz = data['Pelvis_WRT_LabZ']
-# dpelvisz = derivative(pelvisz)
+
+
 # %%
 import numpy as np
-from rms import rms
-n=100
-print(rms(np.array([10+n,4+n,6+n,8+n])))
-
+import matplotlib.pyplot as plt
+x=np.linspace(-2,2,num=100)
+y=x*x
+plt.plot(x,y)
+dydx = np.gradient(y,4/100)
+plt.plot(x,dydx)
+plt.show()
 # %%
